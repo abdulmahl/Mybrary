230b888a9e4e6120cbd888440efe57abee33d9db
Abdul Mahl <mdu_abdul@icloud.com>
2024-01-22 22:04:41 +0200

    Initial Setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const layouts = require("express-ejs-layouts");
+
+const router = require("./routes/index");
+const mongoose = require("mongoose");
+require("dotenv/config");
+const uri = process.env.MONGO_URI;
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(layouts);
+app.use(express.static("public"));
+
+mongoose.connect(uri);
+
+app.use("/", router);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  const db = mongoose.connection;
+  db.on("error", (error) => console.error(error));
+  db.once("open", () => console.log("Connected to the database..."));
+  console.log(`Server listening on port: ${port}...`);
+});
